e6972e9ad73d53995ba2eeabc3ecc0cda17304d2
veronikashikina <veronika.shikina@icloud.com>
2026-06-03 20:42:57 +0300

    Добавлена реализация класса ShoppingList

--- recipes.py
+++ recipes.py
@@ -65,6 +65,51 @@ class Recipe:
         return result
 
 
+class ShoppingList:
+    def __init__(self):
+        self._items = []
+
+    def add_recipe(self, recipe: Recipe, portions: float):
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
+
+        scaled_recipe = recipe.scale(portions)
+        for ingredient in scaled_recipe.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+    def remove_recipe(self, title: str):
+        new_items = []
+        for item in self._items:
+            if item[1] != title:
+                new_items.append(item)
+
+        self._items = new_items
+
+    def get_list(self):
+        working_dict = {}
+        for ingredient, recipe_title in self._items:
+            key = (ingredient.name, ingredient.unit)
+            if key in working_dict:
+                working_dict[key] += ingredient.quantity
+            else:
+                working_dict[key] = ingredient.quantity
+
+        result = []
+        for key in working_dict.keys():
+            result.append(Ingredient(key[0], working_dict[key], key[1]))
+
+        result.sort(key=lambda ingredients: ingredients.name)
+        return result
+
+    def __add__(self, other):
+        new_list = ShoppingList()
+        new_list._items = self._items + other.items
+        return new_list
+
+
+
+
+
 
 
 
